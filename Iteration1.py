981bf91b17fccb56c9bf4b7b617362552a8b2749
Sierrand <90345779+Sierrand@users.noreply.github.com>
2021-10-11 20:01:59 -0700

    Update Iteration1.py

--- Iteration1.py
+++ Iteration1.py
@@ -3,9 +3,13 @@
 
 import streamlit as st
 
-st.header("The First Iteration")
+st.header("Persona 5 Royal Negotiation Guide")
 
-st.write("Persona 5 Royal Negotiation Guide?")
+st.write("An interactive guide to shadow negotiations in Persona 5 Royal")
 
-personaname = st.text_input("Insert a name")
+palaceoption = st.selectbox("Please specify the palace you are in", ("Kamoshida's Palace", "Madarame's Palace"))
+st.write("The current palace is:", palaceoption)
+
+st.write("Please include all - and '")
+personaname = st.text_input("Type name here!")
 st.write("The current persona is:", personaname)
