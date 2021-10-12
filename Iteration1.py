9476519cd6b7e08ede9c9a00e33b77cea87f6875
Sierrand <90345779+Sierrand@users.noreply.github.com>
2021-10-11 20:05:16 -0700

    Update Iteration1.py

--- Iteration1.py
+++ Iteration1.py
@@ -7,6 +7,5 @@ st.write("An interactive guide to shadow negotiations in Persona 5 Royal")
 palaceoption = st.radio("Please specify the palace you are in", ("Kamoshida's Palace", "Madarame's Palace"))
 st.write("The current palace is:", palaceoption)
 
-st.write("Please include all - and '")
 personaname = st.text_input("Type name here!")
 st.write("The current persona is:", personaname)
