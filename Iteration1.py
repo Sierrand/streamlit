d99064100058dcdf36ff6d07b8e9b9456722889a
Sierrand <90345779+Sierrand@users.noreply.github.com>
2021-10-11 20:26:31 -0700

    Update Iteration1.py

--- Iteration1.py
+++ Iteration1.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.header("Persona 5 Royal Negotiation Guide")
 
@@ -9,3 +10,13 @@ st.write("The current palace is:", palaceoption)
 
 personaname = st.text_input("Type name here!")
 st.write("The current persona is:", personaname)
+
+jack_o_lantern1 = pd.DataFrame({"Gloomy":["x", "x", "G"], 
+                                "Irritable":["x", "x", "x"],
+                                "Timid":["G", "x", "x"], 
+                                "Upbeat":["G", "x", "O"]},
+                               index=["You do sound busy.", "Your popularity won't last.", "Who cares?"])
+
+if personaname == "Jack":
+    st.write("I'm busy, ho. It's tough being so popular.")
+    st.write(jack_o_lantern1)
