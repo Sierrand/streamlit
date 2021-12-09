599ba9d80713a99070b2b3f6396152598c45b611
Sierrand <90345779+Sierrand@users.noreply.github.com>
2021-12-08 17:52:49 -0800

    Update P5R.py

--- P5R.py
+++ P5R.py
@@ -9,7 +9,7 @@ st.header("Persona 5 Royal Negotiation Guide")
 st.write("An interactive guide to shadow negotiations in Persona 5 Royal")
 
 palaces = ["None", "Kamoshida's Palace", "..."]
-KArr = (["Jack", "Pixie", "Agathion", "Mandrake", "Bicorn", "Cait Sith", "Incubus", "Kelpie", "Silky", "Succubus", "Angel", "Berith", "Archangel", "Eligor"])
+KArr = ["Jack", "Pixie", "Agathion", "Mandrake", "Bicorn", "Cait Sith", "Incubus", "Kelpie", "Silky", "Succubus", "Angel", "Berith", "Archangel", "Eligor"]
 
 selected = st.selectbox("Would you like to display all of the options from a particular palace?", palaces)
 
